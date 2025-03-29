9f5ca91ec14e3f8295c9b9cbe332508ca4f00bfa
Samuel Nascimento <92036027+samuel21tu@users.noreply.github.com>
2025-03-29 15:50:07 -0300

    1.1

--- 01/02/index.cpp
+++ 01/02/index.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     float anos, cigarros, preço, resultado;
 
-    cout << "a quantos anos você fuma?";
+    cout <<"Calculador de Gastos com Cigarro";
+    cout << "a quantos anos você fuma?\n";
     cin >> anos;
-    cout << "quantos cigarros você fuma por dias?";
+    cout << "quantos cigarros você fuma por dias?\n";
     cin >> cigarros;
-    cout << "qual o preço da carteira de 10 cigarros?";
+    cout << "qual o preço da carteira de 10 cigarros?\n";
     cin >> preço; 
 
     resultado = ((((cigarros*365)/10)*preço)*anos);
 
-    cout << resultado;
-    system("pause");
+    cout << "R$:" << resultado << "\n";
 }
