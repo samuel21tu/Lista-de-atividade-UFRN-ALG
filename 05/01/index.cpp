ca6c170457574aae5409d927a1a88fbd15ed61fa
Samuel Nascimento <samuel321fernandes@gmail.com>
2025-06-04 13:34:13 -0300

    3

--- 05/01/index.cpp
+++ 05/01/index.cpp
@@ -1,17 +1,22 @@
 #include<iostream> 
 using namespace std;
 int main(){
-    int matriz[6][6], vetor[36], i, j ;
+    int matriz[6][6], vetor[36], i, j, a, ;
 
     for(i=0; i<6; i++ ){
         for(j=0; j<6 ;j++){
             cin >> matriz[j][i];
         }
     }
+    cout << "digite um número pra multiplicar o vetor";
+    cin >> a;
+
     for(i=0; i<6; i++ ){
         for(j=0; j<6 ;j++){
-            cout << matriz[j][i];
+            vetor[j]= matriz[j][i]*a;
         }
-        cout << "\n";
+    }
+    for(i=0; i<6; i++ ){
+        cout << vetor[i];
     }
 }
