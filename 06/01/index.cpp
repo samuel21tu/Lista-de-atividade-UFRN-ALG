f3b367c7bd003865c0683e9791fbfc88d1d0804b
Samuel Nascimento <samuel321fernandes@gmail.com>
2025-06-17 16:48:54 -0300

    3.1

---
+++ 06/01/index.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct aluno {
+    int matricula;
+    float mediag;
+    string nome, endereco;
+};
+
+int main(){
+    aluno alunos[10];
+
+    for(int i=1;i<=2;i++){
+        cout << "-------------aluno" <<i << "----------------";
+        cout << "\n\tmatricula: ";
+        cin >> alunos[i].matricula;
+        getchar();
+        cout<< "\tnome: ";
+        getline(cin, alunos[i].nome);
+        cout << "\tendereço: ";
+        getline(cin, alunos[i].endereco);
+        cout << "\tmedia geral: ";
+        cin >> alunos[i].mediag;
+        if(alunos[i].mediag>5){
+            alunos[i].mediag+=0,5;
+        }
+    }
+    for (int i=1;i<=2;i++){
+        cout << alunos[i].matricula <<"\n"<< alunos[i].nome <<"\n"<< alunos[i].endereco <<"\n"<< alunos[i].mediag;
+    }
+
+    return 0;
+}
