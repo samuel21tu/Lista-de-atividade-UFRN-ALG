f4123f784c7c34fb8c1fda796c2a01729023863e
Samuel Nascimento <samuel321fernandes@gmail.com>
2025-06-17 19:31:15 -0300

    3.4

---
+++ 06/15/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std;
+struct registro{
+    int primaryKey; 
+};
+int main(){
+    cout<<"Menu Principal";
+    cout<<"\n\----------------------";
+    cout<<"\t\n1 – INCLUIR UM REGISTRO";
+    cout<<"\t\n2 – BUSCAR UM REGISTRO";
+    cout<<"\t\n3 – ALTERAR UM REGISTRO";
+    cout<<"\t\n4 – EXCLUIR UM REGISTRO";
+    cout<<"\t\n5 – LISTAR REGISTROS EXISTENTES";
+    cout<<"\t\n6 – SAIR DO SISTEMA";
+     
+}
