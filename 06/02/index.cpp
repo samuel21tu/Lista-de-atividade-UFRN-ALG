6c63dabe6bc917ff3350d077c29dbcbf8a6c47b3
Samuel Nascimento <samuel321fernandes@gmail.com>
2025-06-17 18:13:00 -0300

    3.2

--- 06/02/index.cpp
+++ 06/02/index.cpp
@@ -1,6 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-
-struct animal{
-    
+struct aluno {
+    int matricula;
+    float mediag;
+    string nome, endereco;
 };
+
+int main(){
+    aluno alunos[10];
+
+    for(int i=1;i<=2;i++){
+        cout << "-------------aluno" <<i << "----------------";
+        cout << "\n\tmatricula: ";
+        cin >> alunos[i].matricula;
+        getchar();
+        cout<< "\tnome: ";
+        getline(cin, alunos[i].nome);
+        cout << "\tendereço: ";
+        getline(cin, alunos[i].endereco);
+        cout << "\tmedia geral: ";
+        cin >> alunos[i].mediag;
+        if(alunos[i].mediag>5){
+            alunos[i].mediag+=0,5;
+        }
+    }
+    for (int i=1;i<=2;i++){
+        cout <<"matricula: " << alunos[i].matricula <<"\n"<< "nome do aluno: "<< alunos[i].nome <<"\n"<< "ENDEREÇO: " << alunos[i].endereco <<"\n"<<"media geral do aluno: " << alunos[i].mediag << "\n";
+    }
+
+    return 0;
+}
